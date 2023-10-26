af99e8d66f1fd7cdfaf632a61af34db08142d9eb
p1ng-p0ng <studygit@mail.ru>
2023-10-26 14:43:59 +0500

    Lab7

--- ShafikovAutoservice/ServicePage.xaml.cs
+++ ShafikovAutoservice/ServicePage.xaml.cs
@@ -14,12 +14,18 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ShafikovAutoservice
-{
+{  
     /// <summary>
     /// Логика взаимодействия для ServicePage.xaml
     /// </summary>
     public partial class ServicePage : Page
     {
+        int CountRecords;//кол-во записей в табл
+        int CountPage;//Общее кол-во страниц
+        int CurrentPage = 0;//Текущая страница
+
+        List<Service> CurrentPageList = new List<Service>();
+        List<Service> TableList;
         public ServicePage()
         {
             InitializeComponent();
@@ -66,15 +72,123 @@ namespace ShafikovAutoservice
 
             if (RButtonDown.IsChecked.Value)
             {
+                //для отображения итогов фильтра и поиска в листвью по убыванию
                 currentServices = currentServices.OrderByDescending(p => p.Cost).ToList();
             }
 
             if (RButtonUp.IsChecked.Value)
             {
+                //для отображения итогов фильтра и поиска в листвью по возрастанию
                 currentServices = currentServices.OrderBy(p => p.Cost).ToList();
             }
-
+            //Для отображения итогов фильтра и поиска в листвью
+           //currentServices = currentServices.OrderBy
             ServiceListView.ItemsSource = currentServices;
+            //заполнение таблицы для постраничного вывода
+            TableList = currentServices;
+            //Вызов ф-ии отображения кол-ва страниц с парметрами
+            //направление 0 - нач загрузкаа
+            //0 - выбранная стр
+            ChangePage(0, 0);
+        }
+
+        private void ChangePage(int direction, int? SelectedPage)//Ф-я отвечающая за разделение list
+        {
+            //direction - направлние. 0 - нач, 1 - пред страница, 2 - след стр
+            //selectedPage - при нажатии на стрелочки передает null
+            //при выборе определенной страницы в этой перемнной находится номер страницы
+
+            CurrentPageList.Clear();//начальная очистка листа
+            CountRecords = TableList.Count;//определние кол-ва записей во всем списке
+            //определение кол-ва страниц
+            if(CountRecords % 10 > 0)
+            {
+                CountPage = CountRecords / 10 + 1;
+            }
+            else
+            {
+                CountPage = CountRecords / 10;
+            }
+
+            Boolean Ifupdate = true;
+            //Проверка на правильность - если
+            //CurrentPage(номер текущей страницы) "правильный"
+
+            int min;
+
+            if (SelectedPage.HasValue)//проверка на значение не null (тк может быть null)
+            {
+                if(SelectedPage >= 0 && SelectedPage <= CountPage)
+                {
+                    CurrentPage = (int)SelectedPage;
+                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                    for (int i = CurrentPage * 10; i < min; i++)
+                    {
+                        CurrentPageList.Add(TableList[i]);
+                    }
+                }
+            }
+            else //если нажата стрелка
+            {
+                switch(direction) 
+                {
+                    case 1://нажата кнопка "Предыдущая страница"
+                        if(CurrentPage > 0)
+                        //т.е кнопка нажата правильно и "назад" можно идти
+                        {
+                            CurrentPage--;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10 + 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else 
+                        {
+                            Ifupdate = false;
+                            //В случае currentPage попытается выйти из диапазона внесение данных не произойдет
+                        }
+                        break;
+
+                    case 2://нажатая кнопка "след стр"
+                        if(CurrentPage < CountPage - 1)
+                        //если идти вперед можно
+                        {
+                            CurrentPage++;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+                        break;
+                }            
+            }
+
+            if (Ifupdate)//если currentPage не вышел из диапазона, то
+            {
+                PageListBox.Items.Clear();
+                //Удаление старых знач из ListBox номеров стр, нужно, чтобы при изменении
+                //Кол-ва записей стр динамически изменялось
+                for (int i = 1; i <= CountPage; i++)
+                {
+                    PageListBox.Items.Add(i);
+                }
+                PageListBox.SelectedIndex = CurrentPage;
+
+                //вывод кол-ва записей на странице и общ кол-ва
+                min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                TBCount.Text = min.ToString();
+                TBAllRecords.Text = " из " + CountRecords.ToString();
+
+                ServiceListView.ItemsSource = CurrentPageList;
+                //обновить отображение списка услуг
+                ServiceListView.Items.Refresh();
+            }
         }
         //private void Button_Click(object sender, RoutedEventArgs e)
         //{
@@ -118,6 +232,55 @@ namespace ShafikovAutoservice
                 Shafikov_AutoserviceEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
                 ServiceListView.ItemsSource = Shafikov_AutoserviceEntities.GetContext().Service.ToList();
             }
+            UpdateServices();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            //забираем Сервис, для которого нажата кнопка удалить
+            var currentService = (sender as Button).DataContext as Service;
+
+            //Проверка на возможность удаления
+            var currentClientServices = Shafikov_AutoserviceEntities.GetContext().ClientService.ToList();
+            currentClientServices = currentClientServices.Where(p => p.ServiceID == currentService.ID).ToList();
+
+            if (currentClientServices.Count != 0) //если есть записи на этот сервис
+                MessageBox.Show("Невозможно выполнить удаление, так как существуют записи на эту услугу");
+            else
+            {
+                if (MessageBox.Show("Вы точно хотите выполнить удаление?", "Внимание!",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        Shafikov_AutoserviceEntities.GetContext().Service.Remove(currentService);
+                        Shafikov_AutoserviceEntities.GetContext().SaveChanges();
+                        //выводим в листвью измененную табл сервис
+                        ServiceListView.ItemsSource = Shafikov_AutoserviceEntities.GetContext().Service.ToList();
+                        //Чтобы применить фильтры и поиск, если они были на форме изначально
+                        UpdateServices();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+            }
+        }
+
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
+        }
+
+        private void RightDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
+        }
+
+        private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString()) - 1);
         }
     }
 }
